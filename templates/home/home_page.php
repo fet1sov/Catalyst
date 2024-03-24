e19092b5881f531cbb33f02f4b2a4c3fcb575db8
fet1sov <august9222@gmail.com>
2024-03-24 15:27:53 +0500

    Container: added description for the agency langing page
    Added landing page description

--- templates/home/home_page.php
+++ templates/home/home_page.php
@@ -1,5 +1,13 @@
 <link href="_styles/_css/index.css" rel="stylesheet"/>
 
-<div class="description">
+<div class="container">
+    <div class="description-block">
+        <h2>
+            <?= $GLOBALS["locale"]["agencyDescription"]["mainDescription"] ?>
+        </h2>
 
+        <div class="secondary-description">
+            <p><?= $GLOBALS["locale"]["agencyDescription"]["secondaryDescription"] ?></p>
+        </div>
+    </div>
 </div>
