9a721686c988806a419e6b9024c6f5217533df15
fet1sov <august9222@gmail.com>
2024-03-24 05:24:02 +0500

    Basic project set up
    Prepared project

---
+++ templates/elems/header.php
@@ -0,0 +1,10 @@
+<header>
+    <div class="logo">
+        <a href="/"><h1>Catalyst</h1></a>
+    </div>
+    
+    <div class="auth-buttons">
+        <a href="/auth"><?= $GLOBALS["locale"]["navigationBar"]["authButton"] ?></a>
+        <a href="/register"><?= $GLOBALS["locale"]["navigationBar"]["joinButton"] ?></a>
+    </div>
+</header>
