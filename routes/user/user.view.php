d30424df675606585f765c343304b449b6cebc65
fet1sov <august9222@gmail.com>
2024-04-10 00:44:48 +0500

    Profile: Main information
    Profile information

--- routes/user/user.view.php
+++ routes/user/user.view.php
@@ -1,3 +1,6 @@
-<section>
+<link href="_styles/_css/user.css" rel="stylesheet"/>
 
+<section class="user-info">
+    <h2><?= $GLOBALS["locale"]["titles"]["profileTitle"] ?></h2>
+    <div><?= $userData->getUsename() ?></div>
 <section>
