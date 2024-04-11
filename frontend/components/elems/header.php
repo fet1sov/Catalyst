4038b649564ac5591d6e08c5ba773e41a45132f8
fet1sov <august9222@gmail.com>
2024-04-11 22:27:19 +0500

    Profile: Initial markdown
    Profile:
      - Made intial markdown user view

--- frontend/components/elems/header.php
+++ frontend/components/elems/header.php
@@ -1,3 +1,17 @@
+<script>
+    window.addEventListener("DOMContentLoaded", function (event) {
+        window.addEventListener("scroll", function(event) {
+            if (document.body.scrollTop > 80
+            || document.documentElement.scrollTop > 80) {
+                document.querySelector(".navigation").style.backgroundColor = "rgba(255, 255, 255, 0.9);";
+                document.querySelector(".navigation").style.backdropFilter = "blur(10px)";
+            } else {
+                document.querySelector(".navigation").style.backgroundColor = "transparent";
+            }
+        });
+    });
+</script>
+
 <header>
     <nav class="navigation">
         <a class="logo" href="/">
@@ -24,8 +38,10 @@
                     <div class="user-menu">
                         <ul>
                             <li class="logout">
-                                <svg width="800px" height="800px" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" fill="none"><path fill="#000000" fill-rule="evenodd" d="M10.138 1.815A3 3 0 0 1 14 4.688v14.624a3 3 0 0 1-3.862 2.873l-6-1.8A3 3 0 0 1 2 17.512V6.488a3 3 0 0 1 2.138-2.873l6-1.8zM15 4a1 1 0 0 1 1-1h3a3 3 0 0 1 3 3v1a1 1 0 1 1-2 0V6a1 1 0 0 0-1-1h-3a1 1 0 0 1-1-1zm6 12a1 1 0 0 1 1 1v1a3 3 0 0 1-3 3h-3a1 1 0 1 1 0-2h3a1 1 0 0 0 1-1v-1a1 1 0 0 1 1-1zM9 11a1 1 0 1 0 0 2h.001a1 1 0 1 0 0-2H9z" clip-rule="evenodd"/></svg>
-                                <a href="/user/logout"><?= $GLOBALS["locale"]["userMenu"]["logout"] ?></a>
+                                <a href="/user/logout">
+                                    <svg width="800px" height="800px" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" fill="none"><path fill="#000000" fill-rule="evenodd" d="M10.138 1.815A3 3 0 0 1 14 4.688v14.624a3 3 0 0 1-3.862 2.873l-6-1.8A3 3 0 0 1 2 17.512V6.488a3 3 0 0 1 2.138-2.873l6-1.8zM15 4a1 1 0 0 1 1-1h3a3 3 0 0 1 3 3v1a1 1 0 1 1-2 0V6a1 1 0 0 0-1-1h-3a1 1 0 0 1-1-1zm6 12a1 1 0 0 1 1 1v1a3 3 0 0 1-3 3h-3a1 1 0 1 1 0-2h3a1 1 0 0 0 1-1v-1a1 1 0 0 1 1-1zM9 11a1 1 0 1 0 0 2h.001a1 1 0 1 0 0-2H9z" clip-rule="evenodd"/></svg>
+                                    <div><?= $GLOBALS["locale"]["userMenu"]["logout"] ?></div>
+                                </a>
                             </li>
                         </ul>
                     </div>
--- routes/user/user.view.php
+++ routes/user/user.view.php
@@ -1,8 +1,80 @@
 <link href="_styles/_css/user.css" rel="stylesheet"/>
 
+<style>
+    section.user-info {
+        display: flex;
+        align-items: center;
+        justify-content: center;
+    }
+
+    div#profileBlock div#profileInfo {
+        padding: 20px;
+        border-radius: 10px;
+
+        -webkit-box-shadow: 0px 1px 23px 0px rgba(34, 60, 80, 0.2);
+        -moz-box-shadow: 0px 1px 23px 0px rgba(34, 60, 80, 0.2);
+        box-shadow: 0px 1px 23px 0px rgba(34, 60, 80, 0.2);
+    }
+
+    div#profileBlock div#welcomeMessage h2 {
+        font-size: 48px;
+
+        animation-name: appearAnimation-X;
+        animation-duration: 0.5s;
+    }
+
+    div#profileBlock div#profileInfo {
+        animation-name: appearAnimation;
+        animation-duration: 0.5s;
+    }
+
+    div#profileBlock div.profile-block div {
+        display: flex;
+        flex-direction: column;
+    }
+
+    div#profileBlock div h2 {
+        margin-bottom: 20px;
+    }
+</style>
+
+<script>
+    window.addEventListener("DOMContentLoaded", function() {
+        const profileBlock = document.querySelector("div#profileBlock div#profileInfo");
+        const welcomeBlock = document.querySelector("div#profileBlock div#welcomeMessage")
+
+        setInterval(() => {
+            profileBlock.style.display = "block";
+            welcomeBlock.remove();
+        }, 2000);
+    });
+</script>
+
 <section class="user-info">
-    <div>
-        <h2><?= $GLOBALS["locale"]["titles"]["profileTitle"] ?> </h2>
-        <div><?= unserialize($_SESSION["userData"])->getUsername(); ?></div>
+    <div id="profileBlock">
+        <div id="welcomeMessage">
+            <?
+                $time = date("H");
+            ?>
+
+            <? if ($time < "12") { ?>
+                <h2><?= $GLOBALS["locale"]["userPage"]["greetings"]["goodmorning"] ?><?= unserialize($_SESSION["userData"])->getUsername(); ?></h2>
+            <? } else if ($time >= "12" && $time < "17") { ?>
+                <h2><?= $GLOBALS["locale"]["userPage"]["greetings"]["goodday"] ?><?= unserialize($_SESSION["userData"])->getUsername(); ?></h2>
+            <? } else if ($time >= "19") { ?>
+                <h2><?= $GLOBALS["locale"]["userPage"]["greetings"]["goodnight"] ?><?= unserialize($_SESSION["userData"])->getUsername(); ?></h2>
+            <? } ?>
+        </div>
+
+        <div id="profileInfo" style="display: none;">
+            <h2><?= $GLOBALS["locale"]["titles"]["profileTitle"] ?></h2>
+            <div class="profile-block">
+                <img class="small-avatar" style="width: 48px; height: 48px;" src="/api/avatar?userid=<?= unserialize($_SESSION["userData"])->getId(); ?>">
+                <div>
+                    <div style="font-weight: bold;"><?= unserialize($_SESSION["userData"])->getUsername(); ?></div>
+                    <div style="font-size: 12px; color: var(--secondary-color-02);"><?= unserialize($_SESSION["userData"])->getEmail(); ?></div>
+                </div>
+            </div>
+        </div>
     </div>
 <section>
